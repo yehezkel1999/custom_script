fd2acd7831fee7ff3fdcd05f271fee75202eda9b
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-09 18:40:24 +0300

    Update sample_2.sh

--- sample_2.sh
+++ sample_2.sh
@@ -1,2 +1,4 @@
+echo “sample_2.sh: creating file in /home/“
+echo "Yay" > /home/sample_script_2_worked.txt
 echo “sample_2.sh: appending _great to /home/sample_script_1_worked.txt“
 echo "_great" >> /home/sample_script_1_worked.txt
