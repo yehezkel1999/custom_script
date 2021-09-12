63cab305a509dfd1fe5b7b4a6ee17924455b5eef
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-12 11:28:19 +0300

    Update sample_2.sh

--- sample_2.sh
+++ sample_2.sh
@@ -1,4 +1,8 @@
 echo “sample_2.sh: creating -worked- file in /home/“
 echo "Yay" > /home/sample_script_2_worked.txt
+
 echo “sample_2.sh: appending to /home/sample_script.txt“
 echo "_ sample script 2 :)" >> /home/sample_script.txt
+
+echo "sample_1.sh: appending param: \'${global_param1}\' to /home/sample_script.txt"
+echo $global_param1 >> /home/sample_script.txt
