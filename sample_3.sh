20d18ae0e8caf67cd948bce8b4a8f2650706b422
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-12 10:34:09 +0300

    Update sample_3.sh

--- sample_3.sh
+++ sample_3.sh
@@ -1,5 +1,5 @@
 
 echo “sample_3.sh: creating -worked- file in /home/“
 echo "Yay" > /home/sample_script_3_worked.txt
-echo “sample_2.sh: appending to /home/sample_script.txt
+echo “sample_3.sh: appending to /home/sample_script.txt
 echo "~ sample script 3" >> /home/sample_script.txt
