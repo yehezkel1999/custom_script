2348d5b93195965a6b21292914f5afd8d10e181b
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-12 09:45:54 +0300

    Update sample_1.sh

--- sample_1.sh
+++ sample_1.sh
@@ -1,2 +1,3 @@
-echo “sample_1.sh: creating file in /home/“
-echo "sample script :)" > /home/sample_script.txt
+echo “sample_1.sh: creating 2 files in /home/“
+echo "yay" > /home/sample_script_1_worked.txt
+echo "sample script" > /home/sample_script.txt
