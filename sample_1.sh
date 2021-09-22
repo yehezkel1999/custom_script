b5243f6502397ae87ceb7be9c67f667c3c291fbb
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-22 18:06:08 +0300

    Update sample_1.sh

--- sample_1.sh
+++ sample_1.sh
@@ -4,5 +4,5 @@ echo "yay" > /home/sample_script_1_worked.txt
 echo “sample_1.sh: creating file sample_script.txt in /home/“
 echo "= sample script 1" > /home/sample_script.txt
 
-echo "sample_1.sh: appending param: \'${param1}\' to /home/sample_script.txt"
+echo "sample_1.sh: appending param: '$param1' to /home/sample_script.txt"
 echo $param1 >> /home/sample_script.txt
