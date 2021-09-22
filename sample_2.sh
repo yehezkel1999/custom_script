e60ffeba5c5e88056aaedf6cc4f78479ecc5fc34
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-22 18:09:05 +0300

    Update sample_2.sh

--- sample_2.sh
+++ sample_2.sh
@@ -5,5 +5,4 @@ echo “sample_2.sh: appending to /home/sample_script.txt“
 echo "_ sample script 2 :)" >> /home/sample_script.txt
 
 echo "sample_2.sh: appending param: '$global_param' to /home/sample_script.txt"
-echo $global_param
 echo $global_param >> /home/sample_script.txt
