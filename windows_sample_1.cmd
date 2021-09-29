70ea2fd698f9bd3c62cb9d0d89f78551fadc8140
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-29 14:28:54 +0300

    Update windows_sample_1.cmd

--- windows_sample_1.cmd
+++ windows_sample_1.cmd
@@ -1,7 +1,7 @@
-echo “windows_sample_1.cmd: creating -worked- file in /home/“
+echo “windows_sample_1.cmd: creating -worked- file in C:\Users\Administrator\Desktop“
 echo "yay" > C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
 
-echo “windows_sample_1.cmd: creating file sample_script.txt in /home/“
+echo “windows_sample_1.cmd: creating file sample_script.txt in C:\Users\Administrator\Desktop“
 echo "= windows sample script 1" > C:\Users\Administrator\Desktop\sample_script.txt
 
 echo $env:param1
