4cbc1ff12a9a528cac1f4c5e52f3f929a3a886c9
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-29 17:44:08 +0300

    Update windows_sample_1.ps1

--- windows_sample_1.ps1
+++ windows_sample_1.ps1
@@ -1,5 +1,5 @@
-Write-Host “windows_sample_1.cmd: creating -worked- file in C:\Users\Administrator\Desktop“
-$text = 'yay!' | Out-File $text -FilePath C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
+echo “windows_sample_1.cmd: creating -worked- file in C:\Users\Administrator\Desktop“
+echo "yay" > C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
 
-Write-Host “windows_sample_1.cmd: creating file sample_script.txt in C:\Users\Administrator\Desktop“
-$text = '= windows sample script 1' | Out-File $text -FilePath C:\Users\Administrator\Desktop\sample_script.txt
+echo “windows_sample_1.cmd: creating file sample_script.txt in C:\Users\Administrator\Desktop“
+echo "= windows sample script 1" > C:\Users\Administrator\Desktop\sample_script.txt
