a65017f0f42f398d336ea0ffdde0c9039a8a37ff
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-30 18:26:28 +0300

    Update windows_sample_1.ps1

--- windows_sample_1.ps1
+++ windows_sample_1.ps1
@@ -1,4 +1,7 @@
-echo “windows“
-echo "yay" > C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
+echo “windows_sample_1.cmd: creating -worked- file in C:\Users\Administrator\Desktop“
+New-Item C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
+Set-Content C:\Users\Administrator\Desktop\windows_sample_1_worked.txt "yay"
 
-echo "= windows sample script 1" > C:\Users\Administrator\Desktop\sample_script.txt
+echo “windows_sample_1.cmd: creating file sample_script.txt in C:\Users\Administrator\Desktop“
+New-Item C:\Users\Administrator\Desktop\sample_script.txt
+Set-Content C:\Users\Administrator\Desktop\sample_script.txt "= windows sample script 1"
