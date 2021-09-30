bebf16edbc5779596c6ea26f4d48a6c99b60ca08
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-09-30 18:30:33 +0300

    Update windows_sample_2.ps1

--- windows_sample_2.ps1
+++ windows_sample_2.ps1
@@ -1,9 +1,6 @@
-echo “windows_sample_2.cmd: creating -worked- file in C:\Users\Administrator\Desktop“
-echo "yay" > C:\Users\Administrator\Desktop\windows_sample_2_worked.txt
+echo “windows_sample_2.ps1: creating -worked- file in C:\Users\Administrator\Desktop“
+New-Item C:\Users\Administrator\Desktop\windows_sample_2_worked.txt
+Set-Content C:\Users\Administrator\Desktop\windows_sample_2_worked.txt "yay"
 
-echo “windows_sample_2.cmd: creating file sample_script.txt in C:\Users\Administrator\Desktop“
-echo "= windows sample script 2" > C:\Users\Administrator\Desktop\sample_script.txt
-
-echo $env:param1
-echo "windows_sample_2.cmd: appending param: \'${env:param1}\' to C:\Users\Administrator\Desktop\sample_script.txt"
-echo $env:param1 >> C:\Users\Administrator\Desktop\sample_script.txt
+echo “windows_sample_2.ps1: creating file sample_script.txt in C:\Users\Administrator\Desktop“
+Set-Content C:\Users\Administrator\Desktop\sample_script.txt "~ windows sample script 2"
