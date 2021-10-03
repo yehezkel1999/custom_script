a20bd673a5cd960fd5595134c7424b2388b74462
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-03 11:48:14 +0300

    Update windows_sample_2.ps1

--- windows_sample_2.ps1
+++ windows_sample_2.ps1
@@ -1,6 +1,5 @@
-echo “windows_sample_2.ps1: creating -worked- file in C:\Users\Administrator\Desktop“
+
 New-Item C:\Users\Administrator\Desktop\windows_sample_2_worked.txt
 Set-Content C:\Users\Administrator\Desktop\windows_sample_2_worked.txt "yay"
 
-echo “windows_sample_2.ps1: creating file sample_script.txt in C:\Users\Administrator\Desktop“
 Set-Content C:\Users\Administrator\Desktop\sample_script.txt "~ windows sample script 2"
