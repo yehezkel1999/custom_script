27c8230b1e8e972659ad29c9e78d66eb634785a2
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-03 11:40:17 +0300

    Update windows_sample_1.ps1

--- windows_sample_1.ps1
+++ windows_sample_1.ps1
@@ -1,2 +1,7 @@
 
-echo “windows“
+New-Item C:\Users\Administrator\Desktop\windows_sample_1_worked.txt
+Set-Content C:\Users\Administrator\Desktop\windows_sample_1_worked2222.txt "yay"
+
+
+New-Item C:\Users\Administrator\Desktop\sample_script.txt
+Set-Content C:\Users\Administrator\Desktop\sample_script222.txt "= windows sample script 1"
