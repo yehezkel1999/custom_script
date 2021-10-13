9c90442ab655caf759650026f2cfc02622765e96
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-13 12:21:37 +0300

    Update demo_script_1.sh

--- demo_script_1.sh
+++ demo_script_1.sh
@@ -1,7 +1,7 @@
 echo executing script 1
 
-if ! command -v ansible &> /dev/null; then
-    echo *ansible is not installed on this machine*
-else
+if command -v ansible &> /dev/null; then  # if ansible is installed
     ansible --version
+else
+    echo *ansible is not installed on this machine*
 fi
