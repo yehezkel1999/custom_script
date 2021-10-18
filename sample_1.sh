616c1736031f66ac31fc41b389c74301f33036b4
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-18 15:54:35 +0300

    Update sample_1.sh

--- sample_1.sh
+++ sample_1.sh
@@ -5,9 +5,9 @@ echo sample_1.sh: creating file sample_script.txt in /home/
 echo "= sample script 1" > /home/sample_script.txt
 
 echo sample_1.sh: appending param: '$param1' to /home/sample_script.txt
-echo $param1 >> /home/sample_script.txt
+echo "$param1" >> /home/sample_script.txt
 
 echo sample_1.sh: appending param: '$param3' to /home/sample_script.txt
-echo $param3 >> /home/sample_script.txt
+echo "$param3" >> /home/sample_script.txt
 
 yum -y install gcc
