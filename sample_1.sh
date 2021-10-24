8c2636acfce177f3d77f6f8e3943dd96ac48e2c0
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-24 16:05:13 +0300

    Update sample_1.sh

--- sample_1.sh
+++ sample_1.sh
@@ -1,13 +1,7 @@
-echo sample_1.sh: creating -worked- file in /home/
-echo "yay" > /home/sample_script_1_worked.txt
+echo running sample_1.sh
 
-echo sample_1.sh: creating file sample_script.txt in /home/
 echo "= sample script 1" > /home/sample_script.txt
-
-echo sample_1.sh: appending param: '$param1' to /home/sample_script.txt
+echo $param1
 echo "$param1" >> /home/sample_script.txt
-
-echo sample_1.sh: appending param: '$param3' to /home/sample_script.txt
-echo "$param3" >> /home/sample_script.txt
-
-yum -y install gcc
+echo $param2
+echo "$param2" >> /home/sample_script.txt
