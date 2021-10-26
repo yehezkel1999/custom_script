2ebe262d90d941c8f7514bae98a3dcd89e79e727
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-26 15:09:23 +0300

    Create test_5.sh

---
+++ test_5.sh
@@ -0,0 +1,7 @@
+echo running test_5.sh
+
+echo "= sample script 5" > /home/sample_script.txt
+echo $param1
+echo "$param1" >> /home/sample_script.txt
+echo $param3
+echo "$param2" >> /home/sample_script.txt
