30614fdd732f8484f7c5931d54dcd1859faa09bd
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-10-27 16:11:04 +0300

    Update test_5.sh

--- test_5.sh
+++ test_5.sh
@@ -4,4 +4,4 @@ echo "= sample script 5" > /home/sample_script.txt
 echo $param1
 echo "$param1" >> /home/sample_script.txt
 echo $param3
-echo "$param2" >> /home/sample_script.txt
+echo "$param3" >> /home/sample_script.txt
