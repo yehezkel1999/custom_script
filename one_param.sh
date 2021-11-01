ae81ccba7b1b11a8c201f99929cb51a784290681
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-11-01 17:12:34 +0200

    Update one_param.sh

--- one_param.sh
+++ one_param.sh
@@ -1,5 +1,5 @@
 echo one_param.sh
 
-echo "= sample script 1" > /home/sample_script.txt
+echo "= sample script 1" > /root/sample_script.txt
 echo $param1
-echo "$param1" >> /home/sample_script.txt
+echo "$param1" >> /root/sample_script.txt
