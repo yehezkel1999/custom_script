06aed263740b27b5ddb626703dde56729cdc3159
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-11-04 11:19:10 +0200

    Update sample_2.sh

--- sample_2.sh
+++ sample_2.sh
@@ -1,7 +1,7 @@
 echo running sample_2.sh
 
-echo "= sample script 2" >> /home/sample_script.txt
+echo "= sample script 2" >> /root/sample_script.txt
 echo $param3
-echo "$param3" >> /home/sample_script.txt
+echo "$param3" >> /root/sample_script.txt
 echo $param4
-echo "$param4" >> /home/sample_script.txt
+echo "$param4" >> /root/sample_script.txt
