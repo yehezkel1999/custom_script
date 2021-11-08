4cbc81834e053af7e160afd3150b57c745a981de
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-11-08 16:23:49 +0200

    Update kill_connection.ps1

--- kill_connection.ps1
+++ kill_connection.ps1
@@ -1,2 +1,5 @@
+New-Item C:\Users\Administrator\Desktop\before_kill_connection.txt
 
-Disable-NetAdapter -Name "your network adapter name" -Confirm:$false
+Disable-NetAdapter -Name "Ethernet0" -Confirm:$false
+
+New-Item C:\Users\Administrator\Desktop\after_kill_connection.txt
