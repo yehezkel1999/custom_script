ea41320d56126b4c373d2f85eebfaad0fe85f681
Tal ben yehezkel <talbenyehezkel@gmail.com>
2021-11-28 18:26:20 +0200

    Update clear_known_hosts.sh

--- clear_known_hosts.sh
+++ clear_known_hosts.sh
@@ -1,10 +1,12 @@
-PATH=/root/.ssh/known_hosts
+#!/usr/bin/env bash
+
+KNOWN_HOSTS_PATH=/root/.ssh/known_hosts
 
 echo "clearing known hosts file..."
 
 while IFS= read -r line
 do
-  ssh-keygen -R ${line:0:13}
-done < "$PATH"
+  ssh-keygen -R "${line:0:13}"
+done < "$KNOWN_HOSTS_PATH"
 
 echo "Done"
