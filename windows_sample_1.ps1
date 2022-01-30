002216a9f8f286f8fb9deace7fb8022c66bb8553
Tal ben yehezkel <talbenyehezkel@gmail.com>
2022-01-30 15:38:46 +0200

    Update windows_sample_1.ps1

--- windows_sample_1.ps1
+++ windows_sample_1.ps1
@@ -1,3 +1,3 @@
 
 New-Item C:\Users\Administrator\Desktop\sample_script.txt
-Set-Content C:\Users\Administrator\Desktop\sample_script.txt "= windows sample script 1, with parma: $env:param1, $env:parma2"
+Set-Content C:\Users\Administrator\Desktop\sample_script.txt "= windows sample script 1, with param: $env:param1, $env:param2"
