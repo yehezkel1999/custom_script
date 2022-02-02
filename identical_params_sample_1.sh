9ba91103f7189ee7c48443b255b9b6c57b1f527d
Tal Ben Yehezkel <Tal.b@qualisystems.com>
2022-02-02 13:45:37 +0200

    added identical parmas scripts

---
+++ identical_params_sample_1.sh
@@ -0,0 +1,7 @@
+echo running identical_params_sample_1.sh
+
+echo "= sample script 1" > /root/identical_params_sample_script.txt
+echo $param1
+echo "$param1" >> /root/sample_script.txt
+echo $param2
+echo "$param2" >> /root/sample_script.txt
