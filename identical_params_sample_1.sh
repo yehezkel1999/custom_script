38209e4a8a76d490ceccdbcc9270b2a9962ccfe2
Tal Ben Yehezkel <Tal.b@qualisystems.com>
2022-02-06 16:27:35 +0200

    fixed prints and file to append parms

--- identical_params_sample_1.sh
+++ identical_params_sample_1.sh
@@ -1,7 +1,7 @@
 echo running identical_params_sample_1.sh
 
-echo "= sample script 1" > /root/identical_params_sample_script.txt
+echo "= identical params sample script 1" > /root/identical_params_sample_script.txt
 echo $param1
-echo "$param1" >> /root/sample_script.txt
+echo "$param1" >> /root/identical_params_sample_script.txt
 echo $param2
-echo "$param2" >> /root/sample_script.txt
+echo "$param2" >> /root/identical_params_sample_script.txt
--- identical_params_sample_2.sh
+++ identical_params_sample_2.sh
@@ -1,7 +1,7 @@
 echo running identical_params_sample_2.sh
 
-echo "= sample script 2" >> /root/identical_params_sample_script.txt
+echo "= identical params sample script 2" >> /root/identical_params_sample_script.txt
 echo $param2
-echo "$param2" >> /root/sample_script.txt
+echo "$param2" >> /root/identical_params_sample_script.txt
 echo $param3
-echo "$param3" >> /root/sample_script.txt
+echo "$param3" >> /root/identical_params_sample_script.txt
