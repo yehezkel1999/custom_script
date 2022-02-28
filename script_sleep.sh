990e02f7735de72961028e53bd29c6a810001e4f
tal ben yehezkel <talbenyehezkel@gmail.com>
2022-02-28 16:50:57 +0200

    added prints

--- script_sleep.sh
+++ script_sleep.sh
@@ -1,6 +1,8 @@
 echo running script_sleep.sh
 
 echo "~ before sleep script 3" >> /root/before_sleep_script.txt
+echo sleeping
 sleep 20
+echo done.
 echo "~ after sleep script 3" >> /root/after_sleep_script.txt
 
