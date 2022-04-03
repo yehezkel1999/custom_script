2721c23da0aa2c76ce62cfed1ba99148ef64f40c
Tal Ben Yehezkel <talbenyehezkel@gmail.com>
2022-04-03 11:46:20 +0300

    fixed message in file

--- alternate_script_sleep.sh
+++ alternate_script_sleep.sh
@@ -1,8 +1,8 @@
 echo running alternate_script_sleep.sh
 
-echo "~ before sleep script 3" >> /root/before_alternate_script_sleep.txt
+echo "~ before sleep" >> /root/before_alternate_script_sleep.txt
 echo sleeping
 sleep 20
 echo done.
-echo "~ after sleep script 3" >> /root/after_alternate_script_sleep.txt
+echo "~ after sleep" >> /root/after_alternate_script_sleep.txt
 
